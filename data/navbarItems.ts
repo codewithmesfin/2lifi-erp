30944b052264b369500b982544c106d8765ab081
System Administrator <sciemesfin55@gmail.com>
2024-06-01 09:19:25 +0300

    Product Card

--- data/navbarItems.ts
+++ data/navbarItems.ts
@@ -1,10 +1,10 @@
-const navbarItems:any=[
-    { name: "Home", href: "/", current: true },
+const navbarItems: any = [
+  { name: "Home", href: "/", current: true },
+  { name: "Why 2lifi", href: "/whi-2lifi", current: false },
   { name: "Marketplace", href: "/marketplace", current: false },
   { name: "Industries", href: "/industries", current: false },
   { name: "Features", href: "/features", current: false },
   { name: "Testimonials", href: "/testimonials", current: false },
-  { name: "Why 2lifi", href: "/whi-2lifi", current: false },
   { name: "Docs", href: "/docs", current: false },
-  ]
+];
 export default navbarItems;
