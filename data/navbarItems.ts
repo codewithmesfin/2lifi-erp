09164313a762cbb679bb639152031835a59c1f0b
System Administrator <sciemesfin55@gmail.com>
2024-06-05 15:33:41 +0300

    Change the word 2lifi to 2lify

--- data/navbarItems.ts
+++ data/navbarItems.ts
@@ -1,8 +1,8 @@
 const navbarItems: any = [
   { name: "Home", href: "/", current: true },
-  { name: "Why 2lify", href: "/whi-2lify", current: false },
   { name: "Marketplace", href: "/marketplace", current: false },
   { name: "Industries", href: "/industries", current: false },
+  { name: "Why 2lify", href: "/whi-2lify", current: false },
   { name: "Features", href: "/features", current: false },
   { name: "Testimonials", href: "/testimonials", current: false },
   { name: "Docs", href: "/docs", current: false },
